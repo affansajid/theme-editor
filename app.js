3d33b088e5befe6ba04f0c1e9c68b901021e3cb3
Affan Sajid <affansajid@gmail.com>
2016-02-29 01:24:42 -0500

    added mysql module

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var bodyParser = require('body-parser');
 var swig = require('swig');
 
 var routes = require('./routes/index');
-var users = require('./routes/user');
 
 var app = express();
 
@@ -18,9 +17,7 @@ app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
 // view engine setup
-app.engine('swig', swig.renderFile)
-app.set('view cache', false);
-swig.setDefaults({ cache: false });
+app.engine('swig', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'swig');
 
@@ -34,7 +31,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
+// Connect to Database
+
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : '',
+  password : '', 
+  database : '' // database name
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+
+// Close the connection
+
+connection.end(function(err) {
+	// The connection is terminated now
+	if (err) {
+		console.error(err.status);
+	}
+	else {  		
+		console.log("disconnected now");
+	}
+	
+});
 
 /* GET home page. */
 
